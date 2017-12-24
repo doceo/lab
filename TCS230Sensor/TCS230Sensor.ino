60fb675b8078bc86e13b3dc67fcb687db0e85056
Diomede Mazzone <diomede.mazzone@gmail.com>
2017-12-24 15:47:10 +0100

    aggiornamento sensore TCS230s

--- TCS230Sensor/TCS230Sensor.ino
+++ TCS230Sensor/TCS230Sensor.ino
@@ -6,9 +6,9 @@
 // TCS230 or TCS3200 pins wiring to Arduino
 #define S0 4
 #define S1 5
-#define S2 6
-#define S3 7
-#define sensorOut 8
+#define S2 3
+#define S3 2
+#define sensorOut 6
 
 // Stores frequency read by the photodiodes
 int redFrequency = 0;
