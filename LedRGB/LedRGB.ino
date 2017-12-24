a920c7b9a6682105f8048a31b8db6318fc5442d1
Diomede Mazzone <diomede.mazzone@gmail.com>
2017-12-24 15:49:10 +0100

    add led rgb

---
+++ LedRGB/LedRGB.ino
@@ -0,0 +1,27 @@
+int red = A6;
+int green = A5;
+int blue = A7
+
+void setup() 
+{
+pinMode(red, OUTPUT);
+pinMode(green, OUTPUT);
+pinMode(blue, OUTPUT);
+}
+
+
+void loop() 
+{
+ digitalWrite(red, HIGH);
+delay(500);
+digitalWrite(red, LOW);
+delay(500);
+digitalWrite(green, HIGH);
+delay(500);
+digitalWrite(green, LOW);
+delay(500);
+digitalWrite(blue, HIGH);
+delay(500);
+digitalWrite(blue, LOW);
+delay(500);
+}
