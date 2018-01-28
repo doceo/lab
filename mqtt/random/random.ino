24deaec46bc6215f05618fc06a9571d16b51833d
Diomede Mazzone <diomede.mazzone@gmail.com>
2018-01-28 22:47:54 +0100

    insert led

--- mqtt/random/random.ino
+++ mqtt/random/random.ino
@@ -4,10 +4,12 @@ const int threshold = 100;  // threshold value to decide when the detected sound
 #define PIEZO 13
 
 int temp = 900000;
+int ledState = LOW;         // variable used to store the last LED status, to toggle the light
 
 
 void setup() {
   Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
    pinMode(PIEZO, INPUT); 
   //richiamo la funzione che gestisce la connessione
   connectWifi();
@@ -19,10 +21,13 @@ int  sensorReading = analogRead(PIEZO);
 
   // if the sensor reading is greater than the threshold:
   if (sensorReading >= threshold) {
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, HIGH);
     // toggle the status of the ledPin:
     // send the string "Knock!" back to the computer, followed by newline
     Serial.println("Knock!");
   }
-  delay(100);  // delay to avoid overloading the serial port buffer
+  delay(500);  // delay to avoid overloading the serial port buffer
+  digitalWrite(LED_BUILTIN, LOW);
 }
  
