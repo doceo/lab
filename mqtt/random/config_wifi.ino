b44156b0db36515b5cd1bcb25c761c128933ec3d
Diomede Mazzone <diomede.mazzone@gmail.com>
2018-02-01 16:31:26 +0100

    modifiche minori

--- mqtt/random/config_wifi.ino
+++ mqtt/random/config_wifi.ino
@@ -1,6 +1,8 @@
 
 //definisco le costanti che determinano la connessione. Bisogna sostituire l'indirizzo IP della macchina a cui collegarsi.
 
-const char* MY_SSID = "ssid";
-const char* MY_PWD = "pwd";
+const char* ssid = "ssid";
+const char* password = "pwd";
+const char* mqtt_server = "192.168.1.248";
+
 
--- mqtt/random/random.ino
+++ mqtt/random/random.ino
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 const int threshold = 100;  // threshold value to decide when the detected sound is a knock or not
 #define PIEZO 13
@@ -7,17 +8,32 @@ int temp = 900000;
 int ledState = LOW;         // variable used to store the last LED status, to toggle the light
 
 
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
    pinMode(PIEZO, INPUT); 
   //richiamo la funzione che gestisce la connessione
   connectWifi();
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  
 }
  
 void loop() {
   
-int  sensorReading = analogRead(PIEZO);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  int  sensorReading = analogRead(PIEZO);
 
   // if the sensor reading is greater than the threshold:
   if (sensorReading >= threshold) {
@@ -29,5 +45,14 @@ int  sensorReading = analogRead(PIEZO);
   }
   delay(500);  // delay to avoid overloading the serial port buffer
   digitalWrite(LED_BUILTIN, LOW);
+
+
+    snprintf (msg, 75, , value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
+  
+  
 }
  
