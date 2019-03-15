16387ffc060ded9be86c1037323843578b981de6
Diomede Mazzone <diomede.mazzone@gmail.com>
2019-03-15 16:58:24 +0100

    prime modifiche per rilevare le accellerazioni

--- fisica_piano_inclinato/distanza.ino
+++ fisica_piano_inclinato/distanza.ino
@@ -3,27 +3,21 @@
   
   int dist(){
 
- 
-  
-    digitalWrite (TRIGGER, HIGH);                                        // attraverso il trigger inizia a emettere onde
-    delayMicroseconds(10);                                                   // per dieci secondi
-    digitalWrite(TRIGGER,LOW);                                           // e si ferma
 
-  long duration =pulseIn(ECHO, HIGH);                                  //attraverso la funzione pulseIn acquisiamo il segnale tramite il sensore
+tempo1 = 0;
+tempo2 = 0;
+tempo3 = 0;
+tempo4 = 0;
 
-  double distanza = 0.01715 * duration;                                 //calcoliamo la distanza
+for (int i=0; i<5; i++){
 
-//  long int distanza = 0.036 * duration /2;                                 //calcoliamo la distanza
- 
+  digitalWrite (sonarTrigger[i], HIGH);                                        // attraverso il trigger inizia a emettere onde
+  delayMicroseconds(10);                                                   // per dieci secondi
+  digitalWrite(sonarTrigger[i],LOW);                                           // e si ferma
 
-  Serial.println("");
-  Serial.print("istante ");
-  Serial.print(count);
-  Serial.print(" - al millisecondo ");
-  Serial.print(millis());
-  Serial.print(" - durata: ");                                               //stampiamo sul monitor seriale la durata del segnale e la distanza ottenuta
-  Serial.println(duration);
-  Serial.print(" distanza: ");
+  long duration = pulseIn(sonarEcho[i], HIGH);                                  //attraverso la funzione pulseIn acquisiamo il segnale tramite il sensore
+
+  double distanza = 0.01715 * duration; 
 
   if (duration >380000) { 
           Serial.println("fuori portata");                                 //segnaliamo se la distanza è fuori dalla portata dello strumento
@@ -32,19 +26,51 @@
             duration == 1000;
          }
 
-  long int rval = microsecondsToCentimeters(duration);                    //calcoliamo la distanza in centimetri
+  long int rval = microsecondsToCentimeters(duration); 
+  return rval;
+
+  if (rval<5) tempi[i]=millis();
+  
+}
+  
+                                  //calcoliamo la distanza
+
+//  long int distanza = 0.036 * duration /2;                                 //calcoliamo la distanza
+ 
+
+//  Serial.println("");
+//  Serial.print("istante ");
+//  Serial.print(count);
+//  Serial.print(" - al millisecondo ");
+//  Serial.print(millis());
+//  Serial.print(" - durata: ");                                               //stampiamo sul monitor seriale la durata del segnale e la distanza ottenuta
+//  Serial.println(duration);
+//  Serial.print(" distanza: ");
+
+                   //calcoliamo la distanza in centimetri
 
 //////evitiamo una divisione per zero, sostituendo lo zero con 1000
-  Serial.print(distanza); 
-  Serial.println (" cm");
+//  Serial.print(distanza); 
+//  Serial.println (" cm");
 //  Serial.print(rval);
  
- return rval;
+ 
 
 }
 
 long microsecondsToCentimeters(long microseconds)
   {
    return microseconds / cmconv;
+  }
+
+
+double vel(int tratto){
+
+  if (tratto == 1){
+
+
+    
+  }
+  
   }
   
--- fisica_piano_inclinato/fisica_piano_inclinato.ino
+++ fisica_piano_inclinato/fisica_piano_inclinato.ino
@@ -1,21 +1,60 @@
 
+/**********************************************************
 
+la posizione dei sensoti è presa dal più alto al più basso
+posizione 1 2 3 4
+la distanza tra 1-2 e 3-4 è 10 cm
+
+
+***********************************************************/
 
 
 #define MIN_DIST 20
-#define TRIGGER A5
-#define ECHO A4
+
+#define TRIGGER_1 A5
+#define ECHO_1 A4
+
+#define TRIGGER_2 A5
+#define ECHO_2 A4
+
+#define TRIGGER_3 A5
+#define ECHO_3 A4
+
+#define TRIGGER_4 A5
+#define ECHO_4 A4
+
 
 int cmconv = 59; 
 int count = 0;
-  
+int pos;
+
+double v1, v2, acc;
+
+int sonarTrigger[] = {TRIGGER_1, TRIGGER_2, TRIGGER_3, TRIGGER_4};
+int sonarEcho[] = {ECHO_1, ECHO_2, ECHO_3, ECHO_4};
+
+long tempo1, tempo2, tempo3, tempo4;
+
+long tempi[]{tempo1, tempo2, tempo3, tempo4};
+
 void setup() {
 
-    pinMode(TRIGGER, OUTPUT);
-    pinMode(ECHO, INPUT);
+    pinMode(TRIGGER_1, OUTPUT);
+    pinMode(ECHO_1, INPUT);
 
+    pinMode(TRIGGER_2, OUTPUT);
+    pinMode(ECHO_2, INPUT);
+
+    pinMode(TRIGGER_3, OUTPUT);
+    pinMode(ECHO_3, INPUT);
+
+    pinMode(TRIGGER_4, OUTPUT);
+    pinMode(ECHO_4, INPUT);
+
+    
     Serial.begin(115200);  
 //    radio.begin();
+
 
   
 }
@@ -24,9 +63,18 @@ void setup() {
 
 
 void loop() {
-  
-  
-dist();  
+
+Serial.println(count);
+
+//calcoliamo la velocità nel primo e nel secondo tratto
+v1 = vel(1);
+
+v2 = vel(2);
+
+//calcoliamo l'accellerazione
+
+acc = (v1+v2)/2;
+
 count ++;
 delay(30);
 
