73df3944037e797df1001b70a3e552d0f5f97c5f
Diomede Mazzone <diomede.mazzone@gmail.com>
2020-04-08 09:41:19 +0200

    aggiunta cartella python

---
+++ python/eser_001.py
@@ -0,0 +1,33 @@
+x= input("inseriwsci un numero: ")
+y= input("inserisci un altro numero: ")
+
+stringa_uno = input("inserisci una parola: ")
+stringa_due = input("inserisci una seconda parola: ")
+
+if x>y:
+	print(x)
+else:
+	print(y)
+
+if str(stringa_uno)>str(stringa_due):
+
+	print (stringa_uno)
+
+else: 
+	print (stringa_due)
+
+print (len(stringa_uno))
+print (len(stringa_due))
+
+	
+print("la parola più lunga è:")
+
+if len(stringa_uno)>len(stringa_due):
+
+	print (stringa_uno)
+
+else: 
+	print (stringa_due)
+	
+
+
