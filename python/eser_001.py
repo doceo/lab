6c0c9d42fd52ace64115f39096fd24421d9c8dc4
Diomede Mazzone <diomede.mazzone@gmail.com>
2020-04-09 22:14:33 +0200

    esercizio svolto a lezione

--- python/eser_001.py
+++ python/eser_001.py
@@ -1,25 +1,25 @@
-x= input("inseriwsci un numero: ")
-y= input("inserisci un altro numero: ")
+x = input("inserisci un numero: ")
+y = input("inserisci un altro numero: ")
 
 stringa_uno = input("inserisci una parola: ")
 stringa_due = input("inserisci una seconda parola: ")
 
+print("il più grande dei due è: ")
+
 if x>y:
+
 	print(x)
+
 else:
 	print(y)
 
-if str(stringa_uno)>str(stringa_due):
-
-	print (stringa_uno)
-
-else: 
-	print (stringa_due)
-
+print("la lunghezza della prima parola è: ")
 print (len(stringa_uno))
+
+
+print("la lunghezza della seconda parola è: ")
 print (len(stringa_due))
 
-	
 print("la parola più lunga è:")
 
 if len(stringa_uno)>len(stringa_due):
@@ -28,6 +28,4 @@ if len(stringa_uno)>len(stringa_due):
 
 else: 
 	print (stringa_due)
-	
-
 
