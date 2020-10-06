bb2f0bf66d5d314f0b4af0f0372e237efe857fd9
Diomede Mazzone <diomede.mazzone@gmail.com>
2020-10-06 17:40:41 +0200

    organizzazione esercizi python

---
+++ python/esercizi/eser_008_function.py
@@ -0,0 +1,15 @@
+
+#funzione CHIAMATA
+def printme(str):
+	print(str)
+	return
+	
+	
+	
+#funzione CHIAMANTE
+
+print ('inserisci una stringa di caratteri come input')
+stringa = input()
+
+printme(stringa)
+
---
+++ python/teoria/esempio-3.py
@@ -0,0 +1,6 @@
+#python3
+
+nome = input("scrivi il tuo nome.. ")
+
+
+print("ciao",nome )
